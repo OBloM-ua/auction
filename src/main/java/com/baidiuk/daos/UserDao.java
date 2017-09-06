552d7d0dd7cb9fcc9ed1563f996f4986c4e8a014
oleh <oleh12@gmail.com>
2017-09-06 21:17:10 +0200

    Hiberanate configuration + User Entity

---
+++ src/main/java/com/baidiuk/daos/UserDao.java
@@ -0,0 +1,15 @@
+package com.baidiuk.daos;
+
+import com.baidiuk.entities.User;
+
+public class UserDao {
+    public void createOrUpdate(User user){
+
+    }
+    public void delete(User user){
+
+    }
+    public void get(int id){
+
+    }
+}
